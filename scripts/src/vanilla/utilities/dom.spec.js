5ac72aefe6a8d69d70da866a48f9da04fca47d79
Simon Staton <simon.staton@bynd.com>
2017-07-11 19:00:59 +0100

    [feature/unit-tests] Adding unit tests for dom utility

---
+++ scripts/src/vanilla/utilities/dom.spec.js
@@ -0,0 +1,8 @@
+import 'babel-polyfill';
+import DOM from './dom';
+
+describe('dom utility class', () => {
+  it('should check for access to the DOM', async () => {
+    expect(DOM.canUse).toEqual(true); // accessible in jest
+  });
+});
