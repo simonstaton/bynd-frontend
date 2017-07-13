f10883f34f6be9bc9f6a9be7da5111f3285d461b
Simon Staton <simon.staton@bynd.com>
2017-07-13 11:07:54 +0100

    [feature/form-fields-component] Adding FormField component

--- demo/demo.jsx
+++ demo/demo.jsx
@@ -3,31 +3,67 @@ import { render } from 'react-dom';
 import * as Components from '../scripts/react';
 
 class ComponentLoader extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      component: null
+      component: null,
+      propTypes: null,
+      props: {}
     };
   }
+
   loadComponent(e) {
+    const component = Components.default[e.target.value];
+    const propTypes = component.prototype.constructor.propTypes;
     this.setState({
-      component: Components.default[e.target.value]
+      component,
+      propTypes
     });
   }
+
+  setProp(key, e) {
+    const props = this.state.props;
+    props[key] = this.parseProps(e.target.value);
+    this.setState({
+      props: props
+    });
+  }
+
+  parseProps(value) {
+    try {
+      value = (new Function(`return ${value};`))();
+    } catch(err) {}
+    return value;
+  }
+
   render() {
     const Component = this.state.component;
+    const props = this.state.props;
     return (
       <div>
-        <select onChange={this.loadComponent.bind(this)}>
-          <option disabled selected>Select Component</option>
-          {Object.keys(Components.default).map((key, index) => {
-            return <option value={key} key={index}>{key}</option>
+        <fieldset>
+          <legend>Component</legend>
+          <select onChange={this.loadComponent.bind(this)} value="default">
+            <option disabled value="default">Select Component</option>
+            {Object.keys(Components.default).map((key, index) => {
+              let value;
+              const onChange = (e) => value = e.target.value;
+              return <option value={key} value={value} key={index}>{key}</option>
+            })}
+          </select>
+        </fieldset>
+        {Component && this.state.propTypes && <fieldset>
+          <legend>Props</legend>
+          {Object.keys(this.state.propTypes).map((key, index) => {
+            return <div key={index}><label>{key} <input type="text" onBlur={this.setProp.bind(this, key)}/></label></div>
           })}
-        </select>
-        {Component && <Component />}
+        </fieldset>}
+        {Component && <fieldset><Component {...props} /></fieldset>}
       </div>
     );
   }
+
 }
 
 render(
---
+++ scripts/dist/react/components/formField/formField.view.js
@@ -0,0 +1,91 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+exports.Radio = Radio;
+exports.Select = Select;
+exports.default = Input;
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function Radio() {
+  var _this = this;
+
+  var className = this.props.attributes.className;
+  return _react2.default.createElement(
+    "fieldset",
+    { className: this.state.valid ? className : className + " invalid" },
+    this.props.options.map(function (option) {
+      return _react2.default.createElement(
+        "label",
+        { key: option.value, htmlFor: _this.props.attributes.id },
+        _react2.default.createElement("input", _extends({}, _this.props.attributes, _this.state.callbacks, { value: option.value })),
+        option.label
+      );
+    }),
+    !this.state.valid && _react2.default.createElement(
+      "span",
+      { className: "error-msg" },
+      this.props.errorMsg
+    )
+  );
+}
+
+function Select() {
+  var _this2 = this;
+
+  var className = this.props.attributes.className;
+  return _react2.default.createElement(
+    "div",
+    null,
+    _react2.default.createElement(
+      "select",
+      _extends({ ref: function ref(c) {
+          _this2.formElement = c;
+        } }, this.props.attributes, this.state.callbacks, { className: this.state.valid ? className : className + " invalid" }),
+      _react2.default.createElement(
+        "option",
+        { disabled: true, selected: true },
+        this.props.attributes.placeholder
+      ),
+      this.props.options.map(function (option) {
+        return _react2.default.createElement(
+          "option",
+          { key: option.value, value: option.value, disabled: option.disabled },
+          option.label
+        );
+      })
+    ),
+    !this.state.valid && _react2.default.createElement(
+      "span",
+      { className: "error-msg" },
+      this.props.errorMsg
+    )
+  );
+}
+
+function Input() {
+  var _this3 = this;
+
+  var className = this.props.attributes.className;
+  return _react2.default.createElement(
+    "div",
+    null,
+    _react2.default.createElement(this.props.tag, _extends({ ref: function ref(c) {
+        _this3.formElement = c;
+      } }, this.props.attributes, this.state.callbacks, { value: this.state.value, className: this.state.valid ? className : className + " invalid" })),
+    !this.state.valid && _react2.default.createElement(
+      "span",
+      { className: "error-msg" },
+      this.props.errorMsg
+    )
+  );
+}
